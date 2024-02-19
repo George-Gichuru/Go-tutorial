ff2f7e0aa4b1d398f470fd2fef2cb6998338d474
GEORGE-GICHURU <george.gichuru@student.moringaschool.com>
2024-02-19 15:00:50 +0300

    security check

--- main.go
+++ main.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
+type User struct {
+	Username string
+	Password string
+}
+
+var users = map[string]string{
+	"George": "Gichuru02",
+	"User2":  "password2",
+}
+
+func authenticate(username, password string) bool {
+	storedPassword, ok := users[username]
+	if !ok {
+		return false
+	}
+	return storedPassword == password
+}
+
 func main() {
+	username := "George"
+	password := "Gichuru02"
 
-	//strings
-	var nameOne string = "Top G"
-	fmt.Println(nameOne)
-
+	if authenticate(username, password) {
+		fmt.Println("Authentication successful. Granted access")
+	} else {
+		fmt.Println("Authentication failed. Access denied")
+	}
 }
