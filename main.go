9fcd4823058e1a6fa4cc77781a756783ebcdf571
GEORGE-GICHURU <george.gichuru@student.moringaschool.com>
2024-02-20 12:00:14 +0300

    arrays and slices

--- main.go
+++ main.go
@@ -2,31 +2,9 @@ package main
 
 import "fmt"
 
-type User struct {
-	Username string
-	Password string
-}
-
-var users = map[string]string{
-	"George": "Gichuru02",
-	"User2":  "password2",
-}
-
-func authenticate(username, password string) bool {
-	storedPassword, ok := users[username]
-	if !ok {
-		return false
-	}
-	return storedPassword == password
-}
-
 func main() {
-	username := "George"
-	password := "Gichuru02"
+	// arrays
 
-	if authenticate(username, password) {
-		fmt.Println("Authentication successful. Granted access")
-	} else {
-		fmt.Println("Authentication failed. Access denied")
-	}
+	var ages = [3]int{22, 23, 24}
+	fmt.Println(ages, len(ages))
 }
