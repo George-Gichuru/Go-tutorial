8566b9d5597fc8aebac16d3d69277a784698ccf6
GEORGE-GICHURU <george.gichuru@student.moringaschool.com>
2024-02-20 12:08:25 +0300

    arrays and slices

--- main.go
+++ main.go
@@ -6,5 +6,12 @@ func main() {
 	// arrays
 
 	var ages = [3]int{22, 23, 24}
+	names := [4]string{"yoshi", "mario", "peach", "bowser"}
+
 	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
+
+	// slices (use arrays under the hood)
+	var numbers = []int{2, 3, 4}
+	fmt.Println(numbers)
 }
